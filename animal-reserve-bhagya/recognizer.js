bf306fd6d2a82ccc9d359d91fbcc3741f656cb08
BhagyaAriyadasa <bhagya7552@gmail.com>
2019-11-07 13:24:01 +0530

    add db

--- animal-reserve-bhagya/recognizer.js
+++ animal-reserve-bhagya/recognizer.js
@@ -1,10 +1,11 @@
 let AWS = require("aws-sdk");
+const ddb = new AWS.DynamoDB.DocumentClient();
 let rekog = new AWS.Rekognition();
 
-exports.handler = function(event, context, callback) {
+exports.handler = function (event, context, callback) {
     //console.log(JSON.stringify(event, null, 2));
 
-    let s3 = event.Records[0].s3;
+ let s3 = event.Records[0].s3;
     rekog.detectLabels({
         Image: {
             S3Object: {
@@ -14,9 +15,26 @@ exports.handler = function(event, context, callback) {
         },
         MaxLabels: 1
     }).promise()
-    .then(data => {
-        console.log(data);
-        callback(null, {});
-    })
-    .catch(err => callback(err));
+        .then(data => {
+            console.log(data);
+            let label = data.Labels[0].Name;
+
+
+
+
+            ddb.put({
+                TableName: 'plant',
+                Item: { 'Name': s3.object.key, 'label': '@{label{' }
+            }).promise()
+                .then((data) => {
+                    //your logic goes here
+                })
+                .catch((err) => {
+                    //handle error
+                });
+
+
+
+           })
+        .catch(err => callback(err));
 }
